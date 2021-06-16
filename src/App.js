74032bb1ecc6789203c9c6149a4b63e5231284fe
s-kim333 <sejin@student.unimelb.edu.au>
2021-06-17 02:05:09 +1000

    #4.1 create function component, protect with prop-types

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ProopTypes from "prop-types";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   // state
@@ -9,7 +10,8 @@ class App extends React.Component {
     movies: []
   }
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {data : {data : {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading : false });
   }
   // component life cycle
   componentDidMount(){
@@ -20,8 +22,16 @@ class App extends React.Component {
 
   }
   render(){
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading ... " : "Ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? "Loading ... " : movies.map(movie => (
+      <Movie
+        key={movie.id}
+        id={movie.id}
+        year={movie.year}
+        title={movie.title}
+        summary={movie.summary}
+        poster={movie.medium_cover_image}
+      />))}</div>;
   }
 }
 
