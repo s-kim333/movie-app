77831ac7f452b647e686338a38794959a5730a35
s-kim333 <sejin@student.unimelb.edu.au>
2021-06-17 01:15:27 +1000

    #3.1 adding state and planning movie component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from "react";
 import ProopTypes from "prop-types";
 
 class App extends React.Component {
+  // state
+  state = {
+    isLoading : true,
+    movies: []
+  }
   // component life cycle
   componentDidMount(){
 
   }
   componentDidUpdate(){
-    
+
   }
   render(){
-    return <div></div>
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading ... " : "Ready"}</div>;
   }
 }
 
