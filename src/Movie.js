3c245513d38f334b5d5126aaf5effcdb38eff7b7
s-kim333 <sejin@student.unimelb.edu.au>
2021-06-17 02:12:52 +1000

    #4.2 re-write Movie.js and App.js

--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Movie({id,year,title,summary,poster}){
-    return <div>{title}</div>;
-}
+function Movie({ year, title, summary, poster }) {
+    return (
+      <div class="movie">
+        <img src={poster} alt={title} title={title} />
+        <div class="movie__data">
+          <h3 class="movie__title">{title}</h3>
+          <h5 class="movie__year">{year}</h5>
+          <p class="movie__summary">{summary}</p>
+        </div>
+      </div>
+    );
+  }
 
 Movie.propTypes = {
     id : PropTypes.number.isRequired,
