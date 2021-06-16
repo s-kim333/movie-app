3786738d4df34ec06794b38a79224764f61efb24
s-kim333 <sejin@student.unimelb.edu.au>
2021-06-17 00:51:21 +1000

    #2.0 class component structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import React from "react";
+import ProopTypes from "prop-types";
 
-
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+// component; a function returning html 
+class App extends React.Component {
+  render(){
+    return <div></div>
+  }
 }
 
 export default App;
